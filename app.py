4b9718c5c13ed6d642c06fecdce03072cd0d840f
ANDREIP211109 <81253375+ANDREIP211109@users.noreply.github.com>
2022-11-13 19:17:35 +0000

    update app.cpython-310.pyc and app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def hello():
 
 @app.route('/comments/')
 def comments():
-    comments = ['This is the first comment.',
+    commentlist = ['This is the first comment.',
                 'This is the second comment.',
                 'This is the third comment.',
                 'This is the fourth comment.'
                 ]
 
-    return render_template('comments.html', comments=comments)
+    return render_template('comments.html', comments=commentlist)
 
 @app.route('/about/')
 def about():
