e2cccf7b8d60e9c72453b08055ebd7c61970d6df
ANDREIP211109 <81253375+ANDREIP211109@users.noreply.github.com>
2022-11-13 18:55:15 +0000

    update app.cpython-310.pyc, app.py and templates/index.html

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+data={
+    'title': 'Bank'
+}
+
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', data=data)
